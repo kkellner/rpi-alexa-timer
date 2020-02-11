6935e73113b89f630182f701b440c49eaf3e5d39
Kurt Kellner <kurt.kellner@gmail.com>
2020-02-10 20:52:26 -0700

    Update logging

--- alexa-timer-display.py
+++ alexa-timer-display.py
@@ -6,15 +6,17 @@
 
 import logging
 import sys
+import os
 import threading
 import time
 
 import dateutil.parser
 from agt import AlexaGadget
 
-logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+FORMAT = '%(asctime)-15s %(threadName)-10s %(levelname)6s %(message)s'
+logging.basicConfig(stream=sys.stdout, level=logging.NOTSET, format=FORMAT)
+#logging.basicConfig(stream=sys.stdout, level=logging.INFO, format=FORMAT)
 logger = logging.getLogger(__name__)
-
 
 # Alexa Gadget code
 class TimerGadget(AlexaGadget):
@@ -43,23 +45,25 @@ class TimerGadget(AlexaGadget):
         # parse the scheduledTime in the directive. if is already expired, ignore
         t = dateutil.parser.parse(directive.payload.scheduledTime).timestamp()
         if t <= 0:
-            logger.info("Received SetAlert directive but scheduledTime has already passed. Ignoring")
+            logger.info("Received SetAlert directive for token %s but scheduledTime has already passed. Ignoring", directive.payload.token)
             return
 
         # check if this is an update to an alrady running timer (e.g. users asks alexa to add 30s)
         # if it is, just adjust the end time
         if self.timer_token == directive.payload.token:
-            logger.info("Received SetAlert directive to update to currently running timer. Adjusting")
+            logger.info("Received SetAlert directive for token %s to update to currently running timer. Adjusting", directive.payload.token)
             self.timer_end_time = t
             return
 
         # check if another timer is already running. if it is, just ignore this one
         if self.timer_thread is not None and self.timer_thread.isAlive():
-            logger.info("Received SetAlert directive but another timer is already running. Ignoring")
+            logger.info("Received SetAlert directive for token %s but another timer is already running. Ignoring", directive.payload.token)
             return
 
         # start a thread to rotate the servo
-        logger.info("Received SetAlert directive. Starting a timer. " + str(int(t - time.time())) + " seconds left..")
+        logger.info("Received SetAlert directive for token %s. Starting a timer.  %d seconds left.", 
+            directive.payload.token, int(t - time.time()))
+
         self.timer_end_time = t
         self.timer_token = directive.payload.token
 
@@ -89,7 +93,7 @@ class TimerGadget(AlexaGadget):
         start_time = time.time()
         time_remaining = self.timer_end_time - start_time
         while self.timer_token and time_remaining > 0:
-            logger.info("Received SetAlert directive. Starting a timer.  %d seconds left..", time_remaining)
+            logger.info("Timer token %s.  %d seconds left..", self.timer_token, time_remaining)
             time_total = self.timer_end_time - start_time
             time_remaining = max(0, self.timer_end_time - time.time())
 
@@ -104,6 +108,10 @@ class TimerGadget(AlexaGadget):
  
 
 if __name__ == '__main__':
+
+    if os.geteuid() != 0:
+        exit("You need to have root privileges to run this program.\nPlease try again, this time using 'sudo'. Exiting.")
+
     try:
         TimerGadget().main()
     finally:
