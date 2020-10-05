6196a5f38c3a102a166f52fb2ce4da8fd3ec44b1
Kurt Kellner <kurt.kellner@gmail.com>
2020-10-05 14:00:47 -0600

    checkwifi.sh to use gateway ip

--- checkwifi.sh
+++ checkwifi.sh
@@ -6,17 +6,16 @@
 #
 
 GATEWAY=`ip r | grep default | cut -d ' ' -f 3`
-GATEWAY="172.20.0.140"
 ping -q -c 1 -W 10 ${GATEWAY} > /dev/null
 if [ $? != 0 ] 
 then
   # Wait 10 minutes after failed ping to try again
-  sleep 10
+  sleep 600
   ping -q -c 1 -W 10 ${GATEWAY} > /dev/null
   if [ $? != 0 ] 
   then
     echo "Reboot because we can't ping default gateway"
-    # sudo /sbin/shutdown -r now
+    sudo /sbin/shutdown -r now
   fi
 fi
 
